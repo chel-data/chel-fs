3c2afeca11decbedef0a66d0bef10dd9388c5dcc
Paul356 <panhao356@gmail.com>
2024-07-05 17:36:51 +0800

    add missing use declaration

--- build.rs
+++ build.rs
@@ -15,6 +15,9 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
     // Tell cargo to look for shared libraries in the specified directory
     println!("cargo:rustc-link-search=/usr/lib64");
